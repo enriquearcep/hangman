ff41391cccded59c1a2f5583ab80ae3a0337c9dc
Enrique <enriquearce00@gmail.com>
2023-10-15 13:37:35 -0600

    Winner logic

--- Hangman/MainPage.xaml.cs
+++ Hangman/MainPage.xaml.cs
@@ -29,6 +29,18 @@ public partial class MainPage : ContentPage, INotifyPropertyChanged
             OnPropertyChanged();
         }
     }
+
+    private string statusMessage;
+    public string StatusMessage
+    {
+        get => statusMessage;
+
+        set
+        {
+            statusMessage = value;
+            OnPropertyChanged();
+        }
+    }
     #endregion
 
     #region Global variables
@@ -72,8 +84,48 @@ public partial class MainPage : ContentPage, INotifyPropertyChanged
 
 	private void CalculateWord(string answer, List<char> guessed)
 	{
-		var temp = answer.Select(s => (guessed.IndexOf(s) >= 0 ? s : '_')).ToArray();
+		var temp = answer.Select(s => (guessed.Any(a => s.ToString().Equals(a.ToString(), StringComparison.OrdinalIgnoreCase)) ? s : '_')).ToArray();
 
         Spotlight = string.Join(' ', temp);
 	}
+
+    private void ShowAnswerButton_Clicked(object sender, EventArgs e)
+    {
+        CalculateWord(answer, answer.ToArray().ToList());
+    }
+
+    private void KeyboardButton_Clicked(object sender, EventArgs e)
+    {
+        var keyboardButton = sender as Button;
+
+        if (keyboardButton != null)
+        {
+            keyboardButton.IsEnabled = false;
+
+            HandleGuess(keyboardButton.Text.FirstOrDefault());
+        }
+    }
+
+    private void HandleGuess(char letter)
+    {
+        if(!guessed.Any(a => a.ToString().Equals(letter.ToString(), StringComparison.OrdinalIgnoreCase)))
+        {
+            guessed.Add(letter);
+        }
+
+        if(answer.Any(a => a.ToString().Equals(letter.ToString(), StringComparison.OrdinalIgnoreCase)))
+        {
+            CalculateWord(answer, guessed);
+
+            CheckIfGameWon();
+        }
+    }
+
+    private void CheckIfGameWon()
+    {
+        if(Spotlight.Replace(" ", string.Empty).Equals(answer, StringComparison.OrdinalIgnoreCase))
+        {
+            StatusMessage = "¡Has ganado!";
+        }
+    }
 }
