0b799c3817496e4fd824fab400473e56f0773919
Enrique <enriquearce00@gmail.com>
2023-10-15 10:54:47 -0600

    Keyboard + Words dictionary

--- Hangman/MainPage.xaml.cs
+++ Hangman/MainPage.xaml.cs
@@ -1,9 +1,39 @@
-﻿namespace Hangman;
+﻿using System.ComponentModel;
+using System.Diagnostics;
 
-public partial class MainPage : ContentPage
+namespace Hangman;
+
+public partial class MainPage : ContentPage, INotifyPropertyChanged
 {
+	#region Global variables
+	List<string> words = new List<string>();
+	string answer = string.Empty;
+	#endregion
+
 	public MainPage()
 	{
 		InitializeComponent();
+
+		LoadWords();
+
+		SetRandomWord();
+	}
+
+    private void LoadWords()
+    {
+        using var stream = FileSystem.OpenAppPackageFileAsync("spanish.dic").Result;
+        using var reader = new StreamReader(stream);
+
+        while(reader.Peek() is not -1)
+		{
+			words.Add(reader.ReadLine().Split("/").FirstOrDefault().ToUpper());
+		}
+    }
+
+	private void SetRandomWord()
+	{
+		answer = words[new Random().Next(0, words.Count)];
+
+		Debug.WriteLine(answer);
 	}
 }
