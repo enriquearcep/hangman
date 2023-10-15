5d37abc0b20b15596e70f00401f18b3178d5cf4c
Enrique <enriquearce00@gmail.com>
2023-10-15 11:05:56 -0600

    Spotlight

--- Hangman/MainPage.xaml.cs
+++ Hangman/MainPage.xaml.cs
@@ -5,18 +5,37 @@ namespace Hangman;
 
 public partial class MainPage : ContentPage, INotifyPropertyChanged
 {
-	#region Global variables
-	List<string> words = new List<string>();
-	string answer = string.Empty;
-	#endregion
+    #region Properties
+    private string spotlight;
+    public string Spotlight
+    {
+        get => spotlight;
+        
+        set
+        {
+            spotlight = value;
+            OnPropertyChanged();
+        }
+    }
+    #endregion
 
-	public MainPage()
+    #region Global variables
+    List<string> words = new List<string>();
+	string answer = string.Empty;
+    List<char> guessed = new List<char>();
+    #endregion
+
+    public MainPage()
 	{
 		InitializeComponent();
+
+        BindingContext = this;
 
 		LoadWords();
 
 		SetRandomWord();
+
+        CalculateWord(answer, guessed);
 	}
 
     private void LoadWords()
@@ -35,5 +54,12 @@ public partial class MainPage : ContentPage, INotifyPropertyChanged
 		answer = words[new Random().Next(0, words.Count)];
 
 		Debug.WriteLine(answer);
+	}
+
+	private void CalculateWord(string answer, List<char> guessed)
+	{
+		var temp = answer.Select(s => (guessed.IndexOf(s) >= 0 ? s : '_')).ToArray();
+
+        Spotlight = string.Join(' ', temp);
 	}
 }
